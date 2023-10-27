605b3c35b32d4ee0adc357e39d06f8f5b63c3f04
Diego Perez <127640754+dperezhi@users.noreply.github.com>
2023-10-26 17:20:19 -0700

    Phase 2 Messages corrected IOException Branch

--- src/main/model/Dna.java
+++ src/main/model/Dna.java
@@ -46,7 +46,7 @@ public class Dna implements Writable {
                 aminoAcids.add(data[1]);
             }
         } catch (IOException e) {
-            System.out.println(e.getMessage());
+            //System.out.println(e.getMessage());
         }
         return determineAminoAcidChain(codons, aminoAcids);
     }
